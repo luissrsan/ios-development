489e58d75e9b8a2942e787319d1733d1c8ce2ff2
Luis Santiago <luissantiago@macbookair.mynetworksettings.com>
2026-08-06 12:08:38 -0400

    inital commit

---
+++ I am poor/I am poor/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  I am poor
+//
+//  Created by Luis Santiago  on 8/4/26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ I am rich/I am rich/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  I am rich
+//
+//  Created by Luis Santiago  on 7/29/26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
